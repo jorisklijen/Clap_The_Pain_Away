c3401e5819b9497b6d25ef6eb7fa4880a2a07447
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-03-19 00:28:33 +0100

    menu is zo  goed als werkend
    de exti time van een paar anims klopt niet helemaal maa ja t is half 1 nu dus fuck it ik ga pitten

---
+++ Unity/ferdTheGame/Assets/Scripts/MenuScripts/AnimatorFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+	[SerializeField] MennuButtonControler mennuButtonControler;
+	public bool disableOnce;
+
+	void PlaySound(AudioClip whichSound)
+	{
+		if (!disableOnce)
+		{
+			mennuButtonControler.audioSource.PlayOneShot(whichSound);
+		}
+		else
+		{
+			disableOnce = false;
+		}
+	}
+
+}
---
+++ Unity/ferdTheGame/Assets/Scripts/MenuScripts/MennuButtonControler.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MennuButtonControler : MonoBehaviour
+{
+	public int index;
+	[SerializeField] bool keyDown;
+	[SerializeField] int maxIndex;
+	public AudioSource audioSource;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetAxisRaw("Vertical") != 0)
+		{
+			if (!keyDown)
+			{
+
+				//hiero input voor de sensors
+				if (Input.GetAxisRaw("Vertical") < 0)
+				{
+					if (index < maxIndex)
+					{
+						index++;
+					}
+					else
+					{
+						index = 0;
+					}
+
+
+				}
+				//hiero input voor de sensors
+				else if (Input.GetAxisRaw("Vertical") > 0)
+				{
+					if (index > 0)
+					{
+						index--;
+					}
+					else
+					{
+						index = maxIndex;
+					}
+				}
+				keyDown = true;
+			}
+		}
+		else {
+			keyDown = false;
+		}
+    }
+}
