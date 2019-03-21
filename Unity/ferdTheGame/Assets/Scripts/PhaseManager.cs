fe314105a19555f188959613d989f564625f838e
Lunatoid <lunatoid.contact@gmail.com>
2019-03-21 12:49:53 +0100

    PhaseManager werkt bijna

--- Unity/ferdTheGame/Assets/Scripts/PhaseManager.cs
+++ Unity/ferdTheGame/Assets/Scripts/PhaseManager.cs
@@ -23,6 +23,7 @@ public class PhaseManager : MonoBehaviour {
     [SerializeField]
     private List<Phase> phases = new List<Phase>();
     private int currentPhaseIndex = 0;
+    bool prevWasShift = false;
 
     List<GameObject> bgInstances = new List<GameObject>();
 
@@ -34,36 +35,64 @@ public class PhaseManager : MonoBehaviour {
         return phases[currentPhaseIndex];
     }
 
-    private void AddBgInstance(Sprite sprite) {
+    private void AddBgInstance(bool phaseShift = false) {
         // Set all the properties
         GameObject obj = new GameObject();
+        obj.transform.parent = transform;
+
+        string partName = (phaseShift) ? "Fade" : "Solid";
+        obj.name = $"BG Phase {currentPhaseIndex}, {partName}";
         obj.AddComponent<SpriteRenderer>();
+
         SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
-        sr.sprite = sprite;
+        sr.sprite = (phaseShift) ? backgroundTransition : background;
         sr.color = GetCurrentPhase().backgroundColor;
 
         // Set it to around the screen size
-        double width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
-        double height = Camera.main.orthographicSize * 2.0;
-
-        obj.transform.localScale = new Vector3((float)width, (float)height) / 5.0f;
-
+        double width = Camera.main.orthographicSize * Screen.width / Screen.height;
+        obj.transform.localScale = new Vector2((float)width, 1.0f);
+        
         // If there already is an instance, place it above it
         if (bgInstances.Count > 0) {
-            float newY = bgInstances[bgInstances.Count - 1].transform.position.y;
-            newY += (obj.GetComponent<SpriteRenderer>().bounds.extents.y * 2.0f);
-            obj.transform.position = new Vector3(0.0f, newY);
+            float newY = 0.0f;
+
+            if (!prevWasShift) {
+                newY = bgInstances[bgInstances.Count - 1].transform.position.y;
+            } else {
+                // Don't use the phase shift Y because it's larger than the solid bg
+                newY = bgInstances[bgInstances.Count - 2].transform.position.y;
+                newY += background.bounds.extents.y * 2.0f;
+            }
+            
+            if (phaseShift) {
+                newY += backgroundTransition.bounds.extents.y + background.bounds.extents.y;
+            } else {
+                newY += background.bounds.extents.y * 2.0f;
+            }
+
+            float newZ = 0.0f;
+            if (prevWasShift) {
+                newZ = bgInstances[bgInstances.Count - 1].transform.position.z + 1.0f;
+            } else if (phaseShift) {
+                newZ = -1.0f;
+            }
+
+            obj.transform.position = new Vector3(0.0f, newY, newZ);
         }
 
+        prevWasShift = phaseShift;
         bgInstances.Add(obj);
     }
 
     IEnumerator PhaseRoutine() {
         while (currentPhaseIndex < phases.Count) {
             yield return new WaitForSeconds(GetCurrentPhase().phaseDuration);
-            AddBgInstance(backgroundTransition);
+            AddBgInstance(true);
             ++currentPhaseIndex;
         }
+
+        // Make it so the index is at the last phase
+        --currentPhaseIndex;
     }
 
     // Start is called before the first frame update
@@ -71,8 +100,13 @@ public class PhaseManager : MonoBehaviour {
         StartCoroutine(PhaseRoutine());
 
         // Add initial backgrounds
-        AddBgInstance(background);
-        AddBgInstance(background);
+        // @HARDCODED: 4 backgrounds seem to fill the screen
+        AddBgInstance();
+        bgInstances[0].transform.position -= new Vector3(0.0f, 5.0f);
+
+        AddBgInstance();
+        AddBgInstance();
+        AddBgInstance();
     }
     
     // Update is called once per frame
@@ -83,8 +117,10 @@ public class PhaseManager : MonoBehaviour {
                 Destroy(bgInstances[i]);
                 bgInstances.RemoveAt(i);
 
-                // Add next bg
-                AddBgInstance(background);
+                // Add next bg, dirty check name if solid
+                if (bgInstances[i].name.Contains("Solid")) {
+                    AddBgInstance();
+                }
             }
         }
 
